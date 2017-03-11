acfa87726e8be09184a37e130534b79c3b68c7af
Richard Jiang Shenhao <richardejiang@hotmail.com>
2017-03-11 15:13:12 +0800

    initial commit

--- VirtualJoystick/GameScene.swift
+++ VirtualJoystick/GameScene.swift
@@ -14,27 +14,41 @@ class GameScene: SKScene {
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    private let plate = SKSpriteNode(imageNamed: "plate")
+    private let joystick = SKSpriteNode(imageNamed: "top")
+    
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        self.backgroundColor = SKColor.white
+        plate.position = CGPoint(x: self.size.width * 0.1, y: self.size.height * 0.1)
+        plate.size = CGSize(width: self.size.width / 8, height: self.size.width / 8)
+        addChild(plate)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        joystick.size = CGSize(width: plate.size.width / 2, height: plate.size.height / 2)
+        // Note: position is given as center position already
+        joystick.position = CGPoint(x: plate.position.x, y: plate.position.y)
+        joystick.alpha = 0.8
+        addChild(joystick)
+        // Default code by Xcode
+//        // Get label node from scene and store it for use later
+//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+//        if let label = self.label {
+//            label.alpha = 0.0
+//            label.run(SKAction.fadeIn(withDuration: 2.0))
+//        }
+//        
+//        // Create shape node to use during mouse interaction
+//        let w = (self.size.width + self.size.height) * 0.05
+//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+//        
+//        if let spinnyNode = self.spinnyNode {
+//            spinnyNode.lineWidth = 2.5
+//            
+//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
+//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
+//                                              SKAction.fadeOut(withDuration: 0.5),
+//                                              SKAction.removeFromParent()]))
+//        }
     }
     
     
